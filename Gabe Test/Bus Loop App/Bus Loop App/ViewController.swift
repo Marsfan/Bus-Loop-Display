42a27d643fecd980bceb43623eb91c73403e498f
Jaster4000a <jasoncabrejos@hotmail.com>
2017-04-18 16:09:50 -0500

    4-18-17
    Fixed Scanner, Found Low Bluetooth but Sending data is an issue

--- Gabe Test/Bus Loop App/Bus Loop App/ViewController.swift
+++ Gabe Test/Bus Loop App/Bus Loop App/ViewController.swift
@@ -17,21 +17,24 @@ import CoreBluetooth
         var peripherals = Array<CBPeripheral>()
         
         let devName = "CHS Bus Loop"
-        let servUUID = CBUUID(string: "6e400001-b5a3-f393-e0a9-e50e24dcca9e")
+        let servUUID = CBUUID(string: "6E400001-b5A3-F393-E0A9-E50E24DCCA9E")
         let txChar = CBUUID(string: "6E400002-B5A3-F393-E0A9-E50E24DCCA9E")
         let rxChar = CBUUID(string: "6E400003-B5A3-F393-E0A9-E50E24DCCA9E")
         var txCharacteristic: CBCharacteristic?
+        let writeType: CBCharacteristicWriteType = .withoutResponse
         
         override func viewDidLoad() {
             super.viewDidLoad()
-            manager = CBCentralManager(delegate: self, queue: nil)
             textLabel.text="CHS We Are One"
+            manager = CBCentralManager(delegate: self, queue: nil)
             // Do any additional setup after loading the view, typically from a nib.
         }
         
         func centralManagerDidUpdateState(_ central: CBCentralManager) {
             if central.state == CBManagerState.poweredOn {
-                central.scanForPeripherals(withServices: [servUUID], options: nil)
+               // central.scanForPeripherals(withServices: [servUUID], options: nil)
+                central.scanForPeripherals(withServices: nil, options: nil)
+                NSLog("SCANNING")
             } else {
                 print("Bluetooth not available.")
             }
@@ -39,10 +42,13 @@ import CoreBluetooth
         
         
         
-        func centralManager(central: CBCentralManager, didDiscoverperipheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
+        func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+            NSLog("FOUND SOMEGTHING")
             let device = (advertisementData as NSDictionary)
                 .object(forKey: CBAdvertisementDataLocalNameKey)
                 as? NSString
+            
+            
             if device?.contains(devName) == true{
                 self.manager.stopScan()
                 
@@ -52,9 +58,10 @@ import CoreBluetooth
                 manager.connect(peripheral, options: nil)
             }
             
-            func centralManager2(central: CBCentralManager, didConnectperipheral peripheral: CBPeripheral){
+            func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral){
                 peripheral.discoverServices(nil)
             }
+            
             func peripheral(peripheral: CBPeripheral,didDiscoverServices error: NSError?){
                 for service in peripheral.services! {
                     let thisService = service as CBService
@@ -94,11 +101,6 @@ import CoreBluetooth
             // Dispose of any resources that can be recreated.
         }
         
-        
-        //@IBOutlet weak var tableView: UITableView!
-        
-        @IBOutlet weak var deviceSelector: UITableView!
-
         @IBOutlet weak var textField1: UITextField!
         @IBOutlet weak var textField2: UITextField!
         @IBOutlet weak var textField3: UITextField!
@@ -114,13 +116,13 @@ import CoreBluetooth
         @IBOutlet weak var textLabel: UILabel!
         
         @IBAction func UpdateDecimal(_ sender: Any) {
-            var LED1=""
-            var LED2=""
-            var LED3=""
-            var LED4=""
-            var LED5=""
-            var LED6=""
-            var combine=""
+            var LED1=" "
+            var LED2=" "
+            var LED3=" "
+            var LED4=" "
+            var LED5=" "
+            var LED6=" "
+            var combine=" "
             
             if direct1.selectedSegmentIndex == 2{
                 LED1 = "002"
@@ -184,26 +186,10 @@ import CoreBluetooth
                 textLabel.text = combine
             }
             let data = combine.data(using: String.Encoding.utf8)
-            peripheral.writeValue(data!, for: txCharacteristic!,
-                type:CBCharacteristicWriteType.withoutResponse);
-            
+            peripheral.writeValue(data, for txChar: CBCharacteristic, writeType: CBCharacteristicWriteType)
 
         }
 }
 
 
-    /*extension ViewController: UITableViewDataSource{
-     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-     let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-     
-     let peripheral = peripherals[indexPath.row]
-     cell.textLabel?.text = peripheral.name
-     
-     return cell
-     }
-     
-     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-     return peripherals.count
-     }
-     }*/
 
